b8f167e597e0dbab98e6ee603967c941a642d156
uni1224 <saekahane99@gmail.com>
2023-09-02 14:58:54 +0000

    namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,17 @@ devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+namespace :customers do
+  root to: "homes#top"
+  
 
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+namespace :admin do
+  root to: "homes#top"
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
