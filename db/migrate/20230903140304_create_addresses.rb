6112d5bedcb756ac90729bb351fe8923f6200e32
uni1224 <saekahane99@gmail.com>
2023-09-03 14:14:22 +0000

    model作成

---
+++ db/migrate/20230903140304_create_addresses.rb
@@ -0,0 +1,13 @@
+class CreateAddresses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :addresses do |t|
+      t.integer :customer_id
+      t.string :name
+      t.string :address
+      t.string :postal_code
+      
+
+      t.timestamps
+    end
+  end
+end
