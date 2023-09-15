50229d51ca23941901d42d3eb5550c5a69be7b18
uni1224 <saekahane99@gmail.com>
2023-09-15 02:31:49 +0000

    ユーザー側しょうひん

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,14 @@
 class Public::ItemsController < ApplicationController
+    
+    def index
+        @items = Item.all
+        @genres = Genre.all
+    end
+
+    def show
+        @item = Item.find(params[:id])
+        @genres = Genre.all
+    end
+    
+    
 end
