3996c8d1ddc1017a04526e7ec0751197a8d05f35
uni1224 <saekahane99@gmail.com>
2023-10-01 13:27:09 +0000

    ordernew

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,7 @@ class Customer < ApplicationRecord
 
          has_many :cart_items, dependent: :destroy
          has_many :addresses, dependent: :destroy
-         
+
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
@@ -15,5 +15,9 @@ class Customer < ApplicationRecord
   validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
   validates :address, presence: true
   validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
+
+  def customer_name
+    last_name + first_name
+  end
 
 end
