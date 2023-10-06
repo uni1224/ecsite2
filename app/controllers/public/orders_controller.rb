8a10a3a7130f7c27f6b4429de3efb583ddeed35e
uni1224 <saekahane99@gmail.com>
2023-10-06 00:59:35 +0000

    注文詳細画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,6 @@
 class Public::OrdersController < ApplicationController
 
-     def new
+ def new
     @cart_items = current_customer.cart_items
     # カートの中に商品が入っている場合
     if @cart_items.any?
@@ -80,7 +80,7 @@ class Public::OrdersController < ApplicationController
    private
 
   def order_params
-    params.require(:order).permit(:customer_id, :name, :address1, :postal_code, :postage, :billing_amount, :payment_method, :order_status, :address_option,:address_id,:pastal_code,:address)
+    params.require(:order).permit(:customer_id, :name, :address1, :postal_code, :postage, :billing_amount, :payment_method, :order_status)
   end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,9 @@ class Customer < ApplicationRecord
 
          has_many :cart_items, dependent: :destroy
          has_many :addresses, dependent: :destroy
+         has_many :cart_items, dependent: :destroy
+         has_many :orders, dependent: :destroy
+         scope :only_active, -> { where(is_deleted: false) }
 
   validates :last_name, presence: true
   validates :first_name, presence: true
